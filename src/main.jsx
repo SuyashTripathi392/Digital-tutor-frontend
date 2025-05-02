c1e6f204f151ce16a987077aa0253a60b2c456eb
Suyash Tripathi <rajtr3142@gmail.com>
2025-05-02 08:26:16 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { UserContextProvider } from './context/UserContext.jsx'
 import { CourseContextProvider } from './context/CourseContext.jsx'
 
-export const server = 'http://localhost:3000'
+export const server = 'https://digital-tutor-server.onrender.com'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
