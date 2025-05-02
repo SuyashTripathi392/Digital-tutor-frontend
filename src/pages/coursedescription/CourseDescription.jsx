7945778552719d7ee6190692a92ce603c5d0f312
Suyash Tripathi <rajtr3142@gmail.com>
2025-05-02 08:47:04 +0530

    Update CourseDescription.jsx

--- src/pages/coursedescription/CourseDescription.jsx
+++ src/pages/coursedescription/CourseDescription.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "./coursedescription.css";
+import "./courseDescription.css";
 import { useNavigate, useParams } from "react-router-dom";
 import { CourseData } from "../../context/CourseContext";
 import { server } from "../../main";
